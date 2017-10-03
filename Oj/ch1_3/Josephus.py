ae1a5078b03634ce2787f6f7595f1550a3e9ad4f
oaoouo <oaoouo@yeah.net>
2017-10-03 21:53:12 +0800

    update 1003

--- Oj/ch1_3/Josephus.py
+++ Oj/ch1_3/Josephus.py
@@ -1,5 +1,8 @@
 #coding: utf-8
 
+"""
+也可以用双向循环链表来解决, 不过时间复杂度是o(n*m)
+"""
 
 def josephus_1(n, m):
     # 采用递推式求解
@@ -12,14 +15,19 @@ def josephus_1(n, m):
 
 
 def josephus_2(n, m):
+    # 每一轮报完floor(n/m)*m个人后进行递归
+    # 这样递归规模每次就不仅仅是n-1, 而是n-n/m
+    # 从而减少递归调用的次数. 将问题的时间复杂度由o(n)减少至o(logn)
     if (n==1):
         return 0
     if (n < m):
+        # 此时第一轮报数无法删除人数
+        # 所以采用n-1规模的递归解决问题
         return (josephus_2(n-1, m) + m) % n
     result = josephus_2(n-n/m, m) - n%m
     if (result < 0):
-        return result + n
-    return result + result/(m-1)
+        return result + n  # 上一轮最后被删除元素后的编号变换
+    return result + result/(m-1) # 上一轮最后被删除元素前的编号变化
 
 
 if __name__ == '__main__':
